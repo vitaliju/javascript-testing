87b4f2d60f317b5478f9689c0e4af2229ab1b59d
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-09-23 17:07:20 +0300

    update

--- samples/fe-demo/cypress/e2e/spec.cy.js
+++ samples/fe-demo/cypress/e2e/spec.cy.js
@@ -3,23 +3,23 @@ const pageUrl = " http://127.0.0.1:8080/"
 describe('Automated tests for local website works', () => {
   it('the page loads successfully', () => {
     cy.visit(pageUrl);
-  })
+  });
 
   it('display Registration form on load', () => {
     cy.visit(pageUrl);
     cy.get("h2").should('have.text', 'Registration Form');
-    cy.contains('Registration Form');
-    cy.contains('h2', 'Registration Form');
+    //cy.contains('Registration Form');
+    //cy.contains('h2', 'Registration Form');
   });
 
-  it('Name, Gender, Email, Phone, Date of Birth  form fields are visible and empty', () => {
+  it('shows Name, Gender, Email, Phone, Date of Birth  form fields are visible and empty', () => {
     cy.visit(pageUrl);
     cy.get('#name').should('be.visible').and('have.value', '');
     cy.get('#gender').should('be.visible').and('have.value', '');
     cy.get('#email').should('be.visible').and('have.value', '');
     cy.get('#phone').should('be.visible');
     cy.get('#dob').should('be.visible').and('have.value', '');
-  })
+  });
 
   it('display Submit button on load', () => {
     cy.visit(pageUrl);
@@ -31,7 +31,7 @@ describe('Automated tests for local website works', () => {
     cy.get("h3").should('have.text', 'Submitted Information');
   });
 
-  it('Submitted Information table has Name, Gender, Email, Phone, Date of Birth, Age columns', () => {
+  it('shows Submitted Information table has Name, Gender, Email, Phone, Date of Birth, Age columns', () => {
     cy.visit(pageUrl);
     cy.contains('th', 'Name');
     cy.contains('th', 'Gender');
@@ -41,9 +41,17 @@ describe('Automated tests for local website works', () => {
     cy.contains('th', 'Age');
   });
 
-  it('Submitted Information table is empty', () => {
+  it('shows Submitted Information table is empty', () => {
     cy.visit(pageUrl);
     cy.get("tbody").should('have.value', '');
+  });
+
+  it('should not allow form submission with empty fields', () => {
+    cy.visit(pageUrl)
+    cy.get('button').click();
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('Please fill out all fields.');
+    });
   });
 
   it('Allows to fill the data', () => {
@@ -54,26 +62,53 @@ describe('Automated tests for local website works', () => {
     cy.get("#dob").type('1989-01-02');
     cy.get("button").click();
   });
+
+  it('Should display the correct data in the table', () => {
+    cy.visit(pageUrl);
+    cy.get("#name").type('Vitalijus');
+    cy.get("#gender").select('Male');
+    cy.get("#email").type('vitalijus.bielkinas@gmail.com');
+    cy.get("#dob").type('1989-01-02');
+    cy.get("button").click();
+    cy.get('td').should('contain', 'Vitalijus');
+    cy.get('td').should('contain', 'Male');
+    cy.get('td').should('contain', 'vitalijus.bielkinas@gmail.com');
+    cy.get('td').should('contain', '1989-01-02');
+    cy.get('td').should('contain', '35');
+  });
+
+  it('After the sending the registration form gets emptied.', () => {
+    cy.visit(pageUrl);
+    cy.get("#name").type('Vitalijus');
+    cy.get("#gender").select('Male');
+    cy.get("#email").type('vitalijus.bielkinas@gmail.com');
+    cy.get("#dob").type('1989-01-02');
+    cy.get("button").click();
+    cy.get('#name').should('have.value', '');
+    cy.get('#gender').should('have.value', '');
+    cy.get('#email').should('have.value', '');
+    cy.get('#phone').should('be.value', '');
+    cy.get('#dob').should('have.value', '');
+  });
 });
 
-/* Check that all elements load
+//  Check that all elements load
 
-  - Check that Registration title is displayed
+//   - Check that Registration title is displayed
 
-  - Check that form is visible and empty
+//   - Check that form is visible and empty
 
-  - Check that the Submit button is visible.
+//   - Check that the Submit button is visible.
 
-  - The table is visible and empty.
-*/
+//   - The table is visible and empty.
+//
 
-/* Happy path functionlity
+//  Happy path functionlity
 
-  - I am able to fill data and submit it.
+//   - I am able to fill data and submit it.
 
-  - The table contains the data i have sent
+//   - The table contains the data i have sent
 
-  - After the sending the registration form gets emptied.
-*/
+//   - After the sending the registration form gets emptied.
+// 
 
-// Edges cases
